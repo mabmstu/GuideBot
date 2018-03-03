87220b197b3a125b72d93fc30bf6bef53389e841
mabmstu <azelenkova96@mail.ru>
2018-03-03 12:55:24 +0300

    'pyFile'

--- GuideBot.py
+++ GuideBot.py
@@ -19,7 +19,7 @@ dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
 
 # Начинаем поиск обновлений
-updater.start_polling(poll_interval = 1, timeout = 60,bootstrap_retries = 5,clean=True)
+updater.start_polling(poll_interval = 0, timeout = 360,bootstrap_retries = 5,clean=True)
 
 # Останавливаем бота, если были нажаты Ctrl + C
-updater.idle()
+updater.idle(stop_signals=(SIGINT))
