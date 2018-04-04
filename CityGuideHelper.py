3f3e157d19082411ce8be5b42fbefcc6bda2c01e
mabmstu <36813873+mabmstu@users.noreply.github.com>
2018-04-04 16:28:44 +0300

    Delete CityGuideHelper.py

--- CityGuideHelper.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("C:\\my_env\\GuideBot\\Overall.db")
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM sight WHERE city ='Уфа'")
-data = cursor.fetchall()
-print(data)
